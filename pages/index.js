b01b446967935ba8efea8787c417e3e0fc1a418b
brunogeek9 <brunogeek9@gmail.com>
2020-05-06 15:45:05 -0300

    listagem de filmes da API

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,31 @@
 import Head from 'next/head'
+import fetch from 'isomorphic-unfetch'
+const Home = ({ movies }) => {
 
-export default function Home() {
+  console.log(movies)
   return (
     <div className="container">
-      <h1>Essa é a home</h1>
+      {
+        movies.map(movie => (
+          <div>
+            {movie.title}
+          </div>
+        ))
+      }
     </div>
   )
 }
+export async function getServerSideProps() {
+  const { API_URL } = process.env
+
+  const res = await fetch(`${API_URL}/movies`)
+  const data = await res.json()
+
+  return {
+    props: {
+      movies: data
+    }
+  }
+}
+
+export default Home
